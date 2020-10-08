2ff9c08f655141a177d76c674484afff080cd78e
bvictorb <victory-boucher@hotmail.com>
2020-10-08 20:44:48 +0200

    all sections added

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -2,7 +2,7 @@ const sections = document.querySelectorAll('[class^=section-]')
 
 const scrollFunction = () => {
     const scrollPosition = window.scrollY
-    const windowHeight = document.documentElement.clientHeight
+    const windowHeight = document.documentElement.clientHeight / 1.5
     const visibleSections = document.querySelectorAll('.visible')
 
     if (scrollPosition < windowHeight) {
@@ -23,6 +23,21 @@ const scrollFunction = () => {
     } else if (scrollPosition >= 5 * windowHeight && scrollPosition < 6 * windowHeight) {
         removeClass(visibleSections)
         sections[5].classList.add('visible')
+    } else if (scrollPosition >= 6 * windowHeight && scrollPosition < 7 * windowHeight) {
+        removeClass(visibleSections)
+        sections[6].classList.add('visible')
+    } else if (scrollPosition >= 7 * windowHeight && scrollPosition < 8 * windowHeight) {
+        removeClass(visibleSections)
+        sections[7].classList.add('visible')
+    } else if (scrollPosition >= 8 * windowHeight && scrollPosition < 9 * windowHeight) {
+        removeClass(visibleSections)
+        sections[8].classList.add('visible')
+    } else if (scrollPosition >= 9 * windowHeight && scrollPosition < 10 * windowHeight) {
+        removeClass(visibleSections)
+        sections[9].classList.add('visible')
+    } else if (scrollPosition >= 10 * windowHeight && scrollPosition < 11 * windowHeight) {
+        removeClass(visibleSections)
+        sections[10].classList.add('visible')
     }
 }
 
