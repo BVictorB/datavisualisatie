8b866448ad82e65dbaf30dcd1b056f83fb3ab1d3
bvictorb <victory-boucher@hotmail.com>
2020-10-08 15:46:12 +0200

    added alot

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,10 +1,4 @@
-const section0 = document.querySelector('.section-0')
-const section1 = document.querySelector('.section-1')
-const section2 = document.querySelector('.section-2')
-const section3 = document.querySelector('.section-3')
-const section4 = document.querySelector('.section-4')
-const section5 = document.querySelector('.section-5')
-const section6 = document.querySelector('.section-6')
+const sections = document.querySelectorAll('[class^=section-]')
 
 const scrollFunction = () => {
     const scrollPosition = window.scrollY
@@ -13,22 +7,22 @@ const scrollFunction = () => {
 
     if (scrollPosition < windowHeight) {
         removeClass(visibleSections)
-        section0.classList.add('visible')
+        sections[0].classList.add('visible')
     } else if (scrollPosition >= windowHeight && scrollPosition < 2 * windowHeight) {
         removeClass(visibleSections)
-        section1.classList.add('visible')
+        sections[1].classList.add('visible')
     } else if (scrollPosition >= 2 * windowHeight && scrollPosition < 3 * windowHeight) {
         removeClass(visibleSections)
-        section2.classList.add('visible')
+        sections[2].classList.add('visible')
     } else if (scrollPosition >= 3 * windowHeight && scrollPosition < 4 * windowHeight) {
         removeClass(visibleSections)
-        section3.classList.add('visible')
+        sections[3].classList.add('visible')
     } else if (scrollPosition >= 4 * windowHeight && scrollPosition < 5 * windowHeight) {
         removeClass(visibleSections)
-        section4.classList.add('visible')
+        sections[4].classList.add('visible')
     } else if (scrollPosition >= 5 * windowHeight && scrollPosition < 6 * windowHeight) {
         removeClass(visibleSections)
-        section5.classList.add('visible')
+        sections[5].classList.add('visible')
     }
 }
 
